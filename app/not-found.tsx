17b4067155202f7839fadf7d73122e635680002b
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-03-19 23:38:35 +0530

    signin

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <h1 className='font-bold'>Page Not Found: 🤦‍♂️</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
